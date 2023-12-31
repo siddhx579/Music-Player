7c57085e843d35e3b4486212530def646ddfe71b
Siddhant Kapoor <siddhantkapoor579@gmail.com>
2023-12-31 19:05:50 +0530

    Update script.js
    Adding all the const

---
+++ script.js
@@ -0,0 +1,10 @@
+const image = document.getElementById('cover'); 
+const title = document.getElementById('music-title');
+const artist = document.getElementById('music-artist'); const currentTimeEl = document.getElementById('current-time'); 
+const durationEl = document.getElementById('duration'); 
+const progress = document.getElementById('progress'); 
+const playerProgress = document.getElementById('player-progress'); 
+const prevBtn = document.getElementById('prev'); 
+const playBtn = document.getElementById('play'); 
+const nextBtn = document.getElementById('next'); 
+const background = document.getElementById('bg-img');
