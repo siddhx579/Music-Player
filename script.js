12dbff8d31c9e2478f01479cab2d4e75033d7aa0
Siddhant Kapoor <siddhantkapoor579@gmail.com>
2023-12-31 19:12:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play'); 
 const nextBtn = document.getElementById('next'); 
 const background = document.getElementById('bg-img');
+
+const music = new Audio();
+
+const songs = [
+    {
+        path: 'assets/1.mp3', 
+        displayName: 'The Charmer\'s Call',
+        cover: 'assets/1.jpg',
+        artist: 'Hanu Dixit',
+    },
+    {
+        path: 'assets/2.mp3', 
+        displayName: 'You Will Never See Me Coming',
+        cover: 'assets/2.jpg',
+        artist: 'NEFFEX',
+    },
+    {
+        path: 'assets/3.mp3', 
+        displayName: 'Intellect',
+        cover: 'assets/3.jpg',
+        artist: 'yung Logos',
+    }
+];
